9a89372f41465b85e6455bd5945edd660c6949fa
Hoarfroster <hoarfroster@outlook.com>
2023-06-08 03:53:41 +0000

    Fix osAppearance on OSX 10.15 or above

--- macos/Classes/SystemThemePlugin.swift
+++ macos/Classes/SystemThemePlugin.swift
@@ -1,18 +1,40 @@
 import Cocoa
 import FlutterMacOS
 
+private let kAppleInterfaceThemeChangedNotification = "AppleInterfaceThemeChangedNotification"
+private let kAppleInterfaceStyle = "AppleInterfaceStyle"
+private let kAppleInterfaceStyleSwitchesAutomatically = "AppleInterfaceStyleSwitchesAutomatically"
+
 public class SystemThemePlugin: NSObject, FlutterPlugin {
 	public static func register(with registrar: FlutterPluginRegistrar) {
 		let channel = FlutterMethodChannel(name: "system_theme", binaryMessenger: registrar.messenger)
 		let instance = SystemThemePlugin()
 		registrar.addMethodCallDelegate(instance, channel: channel)
 	}
+	
+	func getAppearance(): String {
+        var osAppearance = "Light"
+        if #available(OSX 10.15, *) {
+            let appearanceDescription = NSApplication.shared.effectiveAppearance.debugDescription.lowercased()
+            self.textview.textStorage?.append(NSAttributedString(string: "appearanceDescription: \(appearanceDescription)\n"))
+            if appearanceDescription.contains("dark") {
+                self.osAppearance = "Dark"
+            }
+        } else if #available(OSX 10.14, *) {
+            if let appleInterfaceStyle = UserDefaults.standard.object(forKey: kAppleInterfaceStyle) as? String {
+                self.textview.textStorage?.append(NSAttributedString(string: "appleInterfaceStyle: \(appleInterfaceStyle)\n"))
+                if appleInterfaceStyle.lowercased().contains("dark") {
+                    self.osAppearance = "Dark"
+                }
+            }
+        }
+		return osAppearance;
+    }
 
 	public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
 		switch call.method {
 		case "SystemTheme.darkMode":
-			let type = UserDefaults.standard.string(forKey: "AppleInterfaceStyle") ?? "Light"
-			result(type == "Dark");
+			result(getAppearance() == "Dark");
 		case "SystemTheme.accentColor":
 			if #available(macOS 10.14, *) {
 				if let color = NSColor.controlAccentColor.usingColorSpace(.sRGB) {
