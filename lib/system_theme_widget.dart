76965bd28f6e58e2c60edaca90046f2ffcc79b87
Bruno D'Luka <brunodlukaa@gmail.com>
2023-07-07 11:41:25 -0300

    Resolve example app issues

--- lib/system_theme_widget.dart
+++ lib/system_theme_widget.dart
@@ -1,44 +1,37 @@
-part of  'system_theme.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:system_theme/system_theme.dart';
 
-
-
-typedef ThemeWidgetBuilder = Widget Function(BuildContext context, SystemAccentColor accent);
-
-
+typedef ThemeWidgetBuilder = Widget Function(
+  BuildContext context,
+  SystemAccentColor accent,
+);
 
 class SystemThemedWidget extends StatelessWidget {
-
   final ThemeWidgetBuilder builder;
-  SystemThemedWidget({Key? key,required this.builder}) : super(key: key);
 
-
+  const SystemThemedWidget({Key? key, required this.builder}) : super(key: key);
 
   static const _eventChannel =
-   const EventChannel('system_theme_events/switch_callback');
+      EventChannel('system_theme_events/switch_callback');
 
   static Stream<SystemAccentColor> ThemeStream() {
-    return _eventChannel
-        .receiveBroadcastStream()
-        .map((event) {
-        return SystemAccentColor.fromMap(event);
-  })
-        .distinct();
+    return _eventChannel.receiveBroadcastStream().map((event) {
+      return SystemAccentColor.fromMap(event);
+    }).distinct();
   }
-
-
 
   @override
   Widget build(BuildContext context) {
-
-
     return StreamBuilder(
       stream: ThemeStream(),
       builder: (context, snapshot) {
-          print(snapshot.data);
-          var accent = snapshot.data!=null ? snapshot.data as SystemAccentColor : SystemTheme.accentInstance;
-          return builder(context,accent);
-        }
+        print(snapshot.data);
+        var accent = snapshot.data != null
+            ? snapshot.data as SystemAccentColor
+            : SystemTheme.accentColor;
+        return builder(context, accent);
+      },
     );
   }
 }
-
